f0518e5c89108017f7f5478123c3e6865ffdf92a
Anastasiya Soboleva <soboleva@tiburon-research.ru>
2023-02-06 16:30:55 +0300

    настройка проекта: добавляем babel

--- webpack.config.js
+++ webpack.config.js
@@ -22,4 +22,16 @@ module.exports = {
         new CleanWebpackPlugin(),
 
     ],
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    }
 }
