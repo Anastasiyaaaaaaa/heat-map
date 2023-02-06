405fc865641c26378566e047adbbcd5fb9fe39d3
Anastasiya Soboleva <soboleva@tiburon-research.ru>
2023-02-06 15:34:41 +0300

    настройка webpack: html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
         main: './src/index.js'
     },
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
-    }
+    },
+    plugins: [
+        /** шаблон html, куда подключаем скрипты и стили */
+        new HTMLWebpackPlugin({
+            template: './public/index.html'
+        }),
+
+    ],
 }
