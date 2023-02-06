71c0d0252ae150042092c13a8122275bb11aa79f
Anastasiya Soboleva <soboleva@tiburon-research.ru>
2023-02-06 15:46:46 +0300

    настройка webpack: webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        port: 3000
+    },
     plugins: [
         /** шаблон html, куда подключаем скрипты и стили */
         new HTMLWebpackPlugin({
