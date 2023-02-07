93003dc8e78f1dc59f37202f589fa68da2f274c2
Anastasiya Soboleva <soboleva@tiburon-research.ru>
2023-02-07 10:20:54 +0300

    базовый роутинг

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import './App.css'
 import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom'
+import { HeatMap } from './components/HeatMap'
+import { Content } from './components/Content'
 
 export const App = () => {
-    return <div className='app'>
-        App
-    </div>
+    return  <div className='app'>
+        <Router>
+            <header>
+                <Link to='/'>Home</Link>
+                <Link to='/heat-map'>HeatMap</Link>
+            </header>
+
+            <Switch>
+                <Route path='/heat-map'>
+                    <HeatMap />
+                </Route>
+                <Route path='/'>
+                    <Content />
+                </Route>
+            </Switch>
+
+        </Router>
+    </div> 
 }
